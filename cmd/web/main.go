2c35762bf302746c50f0d7c5ea19dd78d8a28be5
Alex <aleksluciano@gmail.com>
2021-10-10 00:48:33 -0300

    fisrt

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/Aleksluciano/chat386/internal/handlers"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	mux := routes()
+
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
+
+	log.Println("Starting web server on port 8080")
+
+	_ = http.ListenAndServe(":8080", mux)
+
+}
